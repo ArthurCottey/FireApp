4549c1cec0f4a12099a344a9ca09f6ac9369ee2d
Arthur Cottey <arthur.cottey1@gmail.com>
2024-01-11 20:30:48 +0100

    Edit get_users

--- main.py
+++ main.py
@@ -2,4 +2,9 @@ from Class.User import User
 import uuid
 from CRUD.UsersCRUD import *
 
-get_users()
+add_user(User(str(uuid.uuid4()), 'COTILLON', 'Teddy', '14/02/2007'))
+
+list_users = get_users()
+
+for user in list_users:
+    print("User info : ",user.get_name()," ",user.get_first_name()," ",user.get_birth_date())
