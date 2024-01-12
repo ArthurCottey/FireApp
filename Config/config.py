281eea826e730dac813c9d71a3de3980457fd2b9
Arthur Cottey <arthur.cottey1@gmail.com>
2024-01-12 14:33:58 +0100

    Add a config file

---
+++ Config/config.py
@@ -0,0 +1,5 @@
+#region Firebase
+__firebasePath = "Config/FirebaseFile/fir-app-2a5cd-firebase-adminsdk-opycx-67c2eb2a65.json"
+def getFirebasePath():
+    return __firebasePath
+#endregion
--- Database/Database.py
+++ Database/Database.py
@@ -1,7 +1,8 @@
+from Config.config import *
 import firebase_admin
 from firebase_admin import credentials, firestore
-cred = credentials.Certificate("Database/fir-app-2a5cd-firebase-adminsdk-opycx-67c2eb2a65.json")
-firebase_admin.initialize_app(cred)
+__cred = credentials.Certificate(getFirebasePath())
+firebase_admin.initialize_app(__cred)
 
 def database():
     return firestore.client()
