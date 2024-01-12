4275dbb56d74990da47904392798cced656af249
Arthur Cottey <arthur.cottey1@gmail.com>
2024-01-12 14:36:45 +0100

    Edit Database.py

--- Database/Database.py
+++ Database/Database.py
@@ -1,8 +1,13 @@
 from Config.config import *
 import firebase_admin
 from firebase_admin import credentials, firestore
+
+#region Firebase initialization
 __cred = credentials.Certificate(getFirebasePath())
 firebase_admin.initialize_app(__cred)
+#endregion
 
+#region Get Firestore client
 def database():
     return firestore.client()
+#endregion
