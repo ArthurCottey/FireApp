d586ad2a5af6d059f3ae0b13df07ad6ca88cc4c5
Arthur Cottey <arthur.cottey1@gmail.com>
2024-01-12 14:54:00 +0100

    Update the add_user function

--- CRUD/UsersCRUD.py
+++ CRUD/UsersCRUD.py
@@ -1,14 +1,15 @@
 from Database.Database import database
 from Class.User import User
 
-def add_user(user):
+def add_user(first_name, name, birth_date):
     db = database()
-    data_adddb.collection('users').add({
-        'firstName': user.get_first_name(),
-        'name': user.get_name(),
-        'dateOfBirth': user.get_birth_date()
+    data_add = db.collection('users').add({
+        'firstName': first_name,
+        'name': name,
+        'dateOfBirth': birth_date
     })
-
+    user_create = User(data_add[1].id, first_name, name, birth_date)
+    return user_create
 
 
 
--- main.py
+++ main.py
@@ -2,5 +2,8 @@ from Class.User import User
 import uuid
 from CRUD.UsersCRUD import *
 
-list_users = get_users()
+list_users = []
 
+list_users.append(add_user("COTTEY", "Baptiste", "14/12/1995"))
+
+print(list_users[0].get_id()," ",list_users[0].get_name()," ",list_users[0].get_first_name()," ",list_users[0].get_birth_date())
