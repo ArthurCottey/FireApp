764e0899f4a5dfa00ae2a23c99200faccb607080
Arthur Cottey <arthur.cottey1@gmail.com>
2024-01-12 16:27:39 +0100

    Edit main

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from CRUD.UsersCRUD import *
 #region Main program
 list_users = []
 list_users = get_users()
+
 for user in list_users:
     print(user.get_name()," ",user.get_first_name())
 #endregion
