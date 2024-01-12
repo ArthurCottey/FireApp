c5b0ca1e5749132c937178149f5deb78339b1507
Arthur Cottey <arthur.cottey1@gmail.com>
2024-01-12 16:25:23 +0100

    Edit main program

--- main.py
+++ main.py
@@ -2,4 +2,9 @@ from Class.User import User
 import uuid
 from CRUD.UsersCRUD import *
 
+#region Main program
 list_users = []
+list_users = get_users()
+for user in list_users:
+    print(user.get_name()," ",user.get_first_name())
+#endregion
