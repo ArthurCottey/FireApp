3f007ca2aaecb7b9b136db926c0e3099a3c14fd0
Arthur Cottey <arthur.cottey1@gmail.com>
2024-01-12 15:16:28 +0100

    Add function get_users

--- CRUD/UsersCRUD.py
+++ CRUD/UsersCRUD.py
@@ -1,6 +1,7 @@
 from Database.Database import database
 from Class.User import User
 
+#region Function add_user
 def add_user(first_name, name):
     db = database()
     data_add = db.collection('users').add({
@@ -9,6 +10,17 @@ def add_user(first_name, name):
     })
     user_create = User(data_add[1].id, first_name, name)
     return user_create
+#endregion
 
+#region Function get_users
+def get_users():
+    list_users = []
+    db = database()
+    users = db.collection('users').get()
+    for user in users:
+        user_data = user.to_dict()
+        list_users.append(User(user.id, user_data['firstName'], user_data['name']))
+    return list_users
+#endregion
 
 
--- main.py
+++ main.py
@@ -4,5 +4,7 @@ from CRUD.UsersCRUD import *
 
 list_users = []
 
+list_users = get_users()
+
 for user in list_users:
     print(user.get_id(), " ", user.get_name(), " ", user.get_first_name())
